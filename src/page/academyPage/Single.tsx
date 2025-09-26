bc6106e4b14d5d7d498faf2ea94132033efc84c3
Ali Ghasemi <ali.gha3mi75@mail.com>
2025-09-26 15:12:58 +0330

    single page wrapper

---
+++ src/page/academyPage/Single.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import AcademySlider from "./AcademySlider";
+import academyData from "./academyData.json";
+import AcademyDetailsPage from "./AcademyDetailsPage";
+import AcademyPage from "./AcademyPage";
+
+function Single() {
+  return (
+    <AcademyPage />
+    // <div className="bg-gray-100 min-h-screen flex items-center justify-center p-4">
+    //   <AcademySlider name={academyData.name} images={academyData.images} />
+    // </div>
+  );
+}
+
+export default Single;
