1e07c9b80a3d6dc4dcd26035dfe1d8a71ca13bf6
Ali Ghasemi <ali.g4tt@gmail.com>
2025-09-27 12:53:39 +0330

    z-index

--- src/component/page/academyPage/LocationSection.tsx
+++ src/component/page/academyPage/LocationSection.tsx
@@ -32,7 +32,7 @@ const LocationSection: React.FC<LocationSectionProps> = ({
             center={position} 
             zoom={15} 
             scrollWheelZoom={false} 
-            style={{ height: '100%', width: '100%' }}
+            style={{ height: '100%', width: '100%',zIndex:0 }}
           >
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
